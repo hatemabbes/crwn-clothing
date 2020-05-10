701bcf208e6b4a64ee6776bd88396ea2630de112
Hatem <hatem.aabbes@gmail.com>
2020-05-10 16:29:45 +0200

    added react-router-dom and used withReact to send history object to component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import './App.css';
-import HomePage from './pages/homepage/homepage.components';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.components";
+
+const HatsPage = () => (
+  <div>
+    <h1> HatsPage</h1>
+  </div>
+);
 function App() {
   return (
-    <div >
-     <HomePage> </HomePage>
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop/hats" component={HatsPage} />
+      </Switch>
+
+      {/* <HomePage> </HomePage> */}
     </div>
   );
 }
